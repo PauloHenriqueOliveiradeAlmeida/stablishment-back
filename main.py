d552c56c967c1f57d9fabfa41e67910465448eb6
pauloHOAlmeida <paulo.almeida@epiousion.com.br>
2025-05-03 19:30:41 -0300

    :tada: init

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from app.features.stablishment.entrypoints.http.stablishment_routes import (
     stablishment_router,
 )
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-
-router = FastAPI()
+router = FastAPI(title="Estabelecimentos", version="1.0.0")
+router.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 
 router.include_router(stablishment_router)
